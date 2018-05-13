b993dd7fa080bdfeb1d0d267788c1b6b9ee52101
harshita-gupta <gupta.harshita.001@gmail.com>
2018-05-13 08:34:11 -0700

    added basic forms logic

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import AdminLoginForm
 
 
 @app.route('/')
@@ -7,3 +8,9 @@ from app import app
 def index():
     user = {'username': 'Miguel'}
     return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/login')
+def login():
+    form = AdminLoginForm()
+    return render_template('login.html', title='Sign In', form=form)
