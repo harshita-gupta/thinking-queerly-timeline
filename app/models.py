909fb1c007449138faf70df25004bdad275f82c4
harshita-gupta <gupta.harshita.001@gmail.com>
2018-05-13 12:01:12 -0700

    registration and admin panel access works

--- app/models.py
+++ app/models.py
@@ -1,10 +1,43 @@
-from app import db
+from app import db, login
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+
+@login.user_loader
+def load_admin(id):
+    return Admin.query.get(int(id))
+
+
+class Admin(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    workshops_created = db.relationship(
+        'WorkshopActivity', backref='creator', lazy='dynamic')
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class WorkshopActivity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    question = db.Column(db.String(140))
+    unit_is_year = db.Column(db.Boolean(), default=True)
+    admin_owner = db.Column(db.Integer, db.ForeignKey("admin.id"))
+    postits = db.relationship('PostIt', backref='session', lazy='dynamic')
+
+
+class PostIt(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(500))
+    year_timestamp = db.Column(db.Integer())
+    mdy_timestamp = db.Column(db.Date, index=True)
+    on_sex = db.Column(db.Boolean(), default=True)
+    session_id = db.Column(db.Integer, db.ForeignKey('workshop_activity.id'))
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,65 @@
-from flask import render_template
-from app import app
-from app.forms import AdminLoginForm
+from flask import render_template, redirect, flash, url_for
+from app import app, db
+from app.forms import AdminLoginForm, AdminRegistration
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import Admin
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
+    # add code here
     return render_template('index.html', title='Home', user=user)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('admin_panel'))
     form = AdminLoginForm()
+    if form.validate_on_submit():
+        user = Admin.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('admin_panel'))
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/admin_panel')
+@login_required
+def admin_panel():
+    return "you made it to admin panel!"
+
+
+@app.route('/session/<session_id>')
+def session_id_participate(session_id):
+    return ""
+
+
+@app.route('/session/<session_id>/view')
+def session_id_view(session_id):
+    return ""
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('admin_panel'))
+    form = AdminRegistration()
+    if form.validate_on_submit():
+        user = Admin(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered admin!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
